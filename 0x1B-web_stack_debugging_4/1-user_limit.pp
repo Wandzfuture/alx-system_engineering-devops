c4ea0cf0f9caa2cd351d17eafe2d00557f14c1c4
Wandzfuture <michael.zabas@gmail.com>
2024-06-27 20:05:09 +0000

    0x1B. Web stack debugging #4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 # Changes the OS configuration to login with the holberton
 # user and open a file without any error messages
 
-exec { 'OS security config':
-  command => 'sed -i "s/^holberton.*$/holberton    hard    nofile    65535\nholberton    soft    nofile    65535/" /etc/security/limits.conf',
-  path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
